08a7e7061b94d6915abe7e21cf140f97d9273ffd
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2024-03-10 22:56:06 +0300

    no clear

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ echo "Applying database migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
 
 exec gunicorn --bind "0.0.0.0:8000" "mailing_service.wsgi:application"
